23151f54ab532324863887a970c6e6421f40490e
Zenovic Haris <zenovicharis@live.com>
2025-09-14 18:24:33 +0200

    fix: update image hostname in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,26 +6,26 @@ const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	sassOptions: {
-		includePaths: [path.join(__dirname, "styles")],
-	},
-	images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "127.0.0.1",
-				port: "9000",
-				pathname: "/kolpinghaus/**",
-			},
-			{
-				protocol: "https",
-				hostname: "minio-s3.zenovicharis.dev",
-				port: "",
-				pathname: "/**",
-			},
-		],
-	},
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "9000",
+        pathname: "/kolpinghaus/**",
+      },
+      {
+        protocol: "https",
+        hostname: "minio-s3.restaurant-im-kolpinghaus.de",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
